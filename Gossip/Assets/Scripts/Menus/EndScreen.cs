f102e289d55b62d9130c1c349df3a13c9f0b1db2
Quentin <qmartherose@gmail.com>
2024-09-11 16:03:04 +0200

    [Feat] End screen animation

---
+++ Gossip/Assets/Scripts/Menus/EndScreen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Gossip.Menus
+{
+    public class EndScreen : MonoBehaviour
+    {
+        [SerializeField] private Button _QuitButton;
+        [SerializeField] private Text _Message;
+        [SerializeField] private GameObject _GoodMessage;
+
+        [SerializeField] private bool _GoodEnding;
+
+        private const string MESSAGE_START = "Il suffit de ";
+        private const string MESSAGE_END = " personnes pour ruiner la vie d'une autre.";
+        private const string TITLE_CARD_SCENE = "TitleCard";
+
+        void Start()
+        {
+            if (ScoreManager.instance != null) _Message.text = MESSAGE_START + ScoreManager.instance._SpreadCount + MESSAGE_END;
+
+            if (_GoodEnding) _GoodMessage.SetActive(true);
+            else _GoodMessage.SetActive(false);
+
+            _QuitButton.enabled = false;
+            _QuitButton.onClick.AddListener(Quit);
+        }
+
+        private void Quit()
+        {
+            SceneManager.LoadScene(TITLE_CARD_SCENE);
+        }
+
+        private void EnableQuit()
+        {
+            _QuitButton.enabled = true;
+        }
+    }
+}
+
