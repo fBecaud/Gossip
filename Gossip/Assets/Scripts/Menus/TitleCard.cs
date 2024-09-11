c8c4038a0b59f08e3688124bbd013c42e22296cf
Quentin <qmartherose@gmail.com>
2024-09-11 10:51:44 +0200

    [Feat] TitleCard, Credit placeholders

--- Gossip/Assets/Scripts/Menus/TitleCard.cs
+++ Gossip/Assets/Scripts/Menus/TitleCard.cs
@@ -13,17 +13,26 @@ namespace Gossip.Menus
         [SerializeField] private Button _CreditButton;
         [SerializeField] private Button _QuitButton;
 
+        [SerializeField] private Image _TransitionScreen;
+        [SerializeField] private float _TransitionSpeed;
+
+        private float _ElapsedTime;
+
         private const string GAME_SCENE = "Main";
         private const string CREDIT_SCENE = "Credit";
 
         void Start()
         {
-
+            _PlayButton.onClick.AddListener(Play);
+            _SettingsButton.onClick.AddListener(DisplaySettings);
+            _CreditButton.onClick.AddListener(Credit);
+            _QuitButton.onClick.AddListener(Quit);
         }
 
         private void Play()
         {
-            SceneManager.LoadScene(GAME_SCENE);
+            _TransitionScreen.gameObject.SetActive(true);
+            StartCoroutine(TransitionCoroutine());
         }
 
         private void DisplaySettings()
@@ -40,6 +49,20 @@ namespace Gossip.Menus
         {
             Application.Quit();
         } 
+
+        private IEnumerator TransitionCoroutine()
+        {
+            while (_TransitionScreen.color != Color.black)
+            {
+                _ElapsedTime += Time.deltaTime;
+                _TransitionScreen.color = Color.Lerp(Color.clear, Color.black, _ElapsedTime * _TransitionSpeed);
+                yield return new WaitForEndOfFrame();
+            }
+
+            SceneManager.LoadScene(GAME_SCENE);
+
+            StopCoroutine(TransitionCoroutine());
+        }
     }
 }
 
